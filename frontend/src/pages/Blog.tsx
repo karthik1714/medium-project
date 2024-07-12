1360230d22e7c9b133a0d024f59be8cdf52b205d
Your Name <you@example.com>
2024-07-12 21:49:09 +0530

    corrected

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,29 +4,30 @@ import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 import BlogSkeleton from "../components/Blogskeleton";
 
-// atomFamilies/selectorFamilies
- const Blog = () => {
-    const { id } = useParams();
-    const {loading, blog} = useBlog({
-        id: id || ""
-    });
-    if (loading) {
-      return <div>
-          <Appbar /> 
-          <div  className="flex justify-center">
-              <div>
-                  <BlogSkeleton />
-                  <BlogSkeleton />
-                  <BlogSkeleton />
-                  <BlogSkeleton />
-                  <BlogSkeleton />
-              </div>
-          </div>
-      </div>
-  }
-    return (
-      <FullBlog blog={blog} />
-    );
-    }
+const BlogPage = () => {
+  const { id } = useParams();
+  const { loading, blog } = useBlog({
+    id: id || ""
+  });
 
- export default Blog
+  if (loading || !blog) { // Handle the case where blog is null or undefined
+    return (
+      <div>
+        <Appbar /> 
+        <div className="flex justify-center">
+          <div>
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <FullBlog blog={blog} />;
+};
+
+export default BlogPage;
